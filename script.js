18cdd9c5245fb5b79f06e5215c1b031e2429019b
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-10-23 08:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+let arr = [2,1,2,1,1,1,2,3,1]
+alert(getMax(arr))
+function getMax(arr) {
+	let max = 0;
+	let maxEle = 0;
+	for (let i = 0; i < arr.length-1; i++) {
+		let count = 0;
+		for (let j = i+1; j < arr.length; j++) {
+			if (arr[i] === arr[j]) {
+				count++
+			}
+		}
+		if (count > max) {
+			max = count;
+			maxEle = arr[i];
+		}
+	}
+	if ( max >= Math.floor(arr.length/2) ) {
+		return maxEle
+	}
+	else{
+		return -1
+	}
+}
